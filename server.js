b0ffa19445dbafee3f718c1c8c39a67938e1d7ac
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-10-05 12:32:40 +0200

    update express to serve static files from public folder

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 let app = express();
 
-app.get('/', (req, res) => res.send('hello express'));
+// app.get('/', (req, res) => res.send('hello nodemon'));
+
+app.use(express.static('public'));
 
 app.listen(3000);
