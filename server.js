0d90957850dc419b4d17cba8d11e53a6abc89815
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-10-05 16:36:27 +0200

    remove bundle and add mongo

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
 
 dotenv.config();
 
 let app = express();
-
-const mongoUrl = process.env.MONGO_URL;
 
 // app.get('/', (req, res) => res.send('hello nodemon'));
 
 app.use(express.static('public'));
 
 app.listen(3000);
+
+MongoClient.connect(process.env.MONGO_URL, (err, database) => {
+  if (err) throw err;
+
+  // const db = database.db('rgrjs');
+  // console.log("Connected successfully to server");  
+  
+  database.db().collection('links').find({}).toArray((err, links) => {
+    if (err) throw err;
+
+    console.log(links);
+  })
+});
