01b0ea89e4f157327db1688a4df18a0f8e514f4e
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-10-05 12:50:28 +0200

    add some stupid state

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,13 @@
 // console.log(React);
 class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'Hello from React'
+    }
+  }
   render() {
-    return React.createElement('h3', null, 'Hello from React!');
+    return React.createElement('h3', null, this.state.message);
   }
 }
 
