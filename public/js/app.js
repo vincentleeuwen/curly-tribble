24baee52cd904c9b342d988f329d5aaa93b12241
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-10-05 17:40:10 +0200

    add store as EventEmitter subclass and Listen for events in main component

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import API from './api';
+import LinkStore from './stores/LinkStore';
+
+const _getAppState = () => {
+  return { links: LinkStore.getAll() };
+}
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = _getAppState();
+    this.onChange = this.onChange.bind(this);
+  }
   async componentDidMount() {
+    LinkStore.on('change', this.onChange);
     await API.fetchLinks();
-    // console.log(links);
+  }
+  componentWillUnmount() {
+    LinkStore.removeListener('change', this.onChange);
+  }
+  onChange() {
+    console.log('4. In the view');
+    this.setState(_getAppState());
   }
   render() {
     return (
