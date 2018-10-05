6a96221459e3493b9f31e3122a348a3fa51c5633
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-10-05 16:13:20 +0200

    add dotenv to load in mongo urls

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 let app = express();
+
+const mongoUrl = process.env.MONGO_URL;
 
 // app.get('/', (req, res) => res.send('hello nodemon'));
 
