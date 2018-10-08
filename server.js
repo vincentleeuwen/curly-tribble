eb8f58b6fbeada9520b3d807167f2e7fcee79f94
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-10-08 22:55:51 +0200

    refactor server to use async / await

--- server.js
+++ server.js
@@ -12,24 +12,12 @@ app.use(express.static('public'));
 
 const port = 3000;
 
-let db;
-MongoClient.connect(process.env.MONGO_URL, (err, database) => {
-  if (err) throw err;
-
-  db = database.db();
+(async () => {
+  const database = await MongoClient.connect(process.env.MONGO_URL);
+  const db = database.db();
   app.use('/graphql', GraphQLHTTP({
     schema: Schema(db),
     graphiql: true
   }));
-
   app.listen(port, () => console.log(`Listening on port ${port}...`));
-  // console.log("Connected successfully to server");  
-});
-
-// app.get('/data/links', (req, res) => {
-//   db.collection('links').find({}).toArray((err, links) => {
-//     if (err) throw err;
-
-//     res.json(links);
-//   })
-// });
+})();
