5ef0e8852f108f941e2ab2615e37a802fb1ddfbe
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-10-08 22:55:19 +0200

    refactor Main component

---
+++ public/js/components/Main.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import API from '../api';
+import LinkStore from '../stores/LinkStore';
+import 'babel-polyfill';
+
+const _getAppState = () => {
+  return { links: LinkStore.getAll() };
+};
+
+class Main extends Component {
+  static propTypes = {
+    limit: PropTypes.number
+  }
+  static defaultProps = {
+    limit: 5
+  }
+  state = _getAppState();
+
+  async componentDidMount() {
+    LinkStore.on('change', this.onChange);
+    await API.fetchLinks();
+  }
+  componentWillUnmount() {
+    LinkStore.removeListener('change', this.onChange);
+  }
+  onChange = () => {
+    this.setState(_getAppState());
+  }
+  render() {
+    const { limit } = this.props;
+    let content = this.state.links.slice(0, limit).map(link => {
+      return (
+        <li key={link._id}>
+          <a href={link.url} target="_blank">{link.title}</a>
+        </li>
+      )
+    });
+    return (
+      <div>
+        <h3>Links</h3>
+        <ul>
+          { content }
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Main;
